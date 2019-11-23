17822c461e014472b3563b33c76c2e6127e8ff6b
Sotirios Pupakis <sotirios.pupakis@gmail.com>
2019-11-23 04:29:53 +0100

    Almost working
    One last thing and it is ready for testing

--- proj2.c
+++ proj2.c
@@ -3,11 +3,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Defines
+#define I_0 10.0E-12
+#define U_T 25.8563E-3
+
 /*
- * Created by Sotirios Pupakis
+ * Author: Sotirios Pupakis
  * Created on 18.11.2019
  * Last modified: 18.11.2019
- * Project 1: Projekt 2 - Iteration computation
+ * Project 1: Project 2 - Iteration computation
  * Login: xpupak01
  * Bonus: None
  * Known bugs: TBA
@@ -15,12 +19,17 @@
 
 /*
  * Exit codes:
- * 0    -   Everything OK
- * -1   -   Not enough or too many parameters provided  -   Line: 29
- * -2   -   Error while parsing arguments               -   Line: 45
+ *  0    -   Everything OK
+ * -1   -   Not enough or too many parameters provided
+ * -2   -   Error while parsing arguments
+ * -3   -   Resistivity is 0
+ * -4   -   Unidentified error
  */
 
-double loadArgument(char *arg);     // Checks argument and returns it's double value
+double loadArgument(char *arg);                                 // Checks argument and returns it's double value
+double diode(double u0, double r, double eps);                  // Calculates U_P based on provided parameters
+double getUPResult(double U_P, double U_0, double R);           // Returns the result with given U_P
+double calculateAccurateUP(double U_0, double R, double eps);   // Returns U_P that is approximated using bisection method
 
 int main(int argc, char *argv[]) {
     // Check if we got enough arguments
@@ -30,16 +39,41 @@ int main(int argc, char *argv[]) {
     }
 
     // Safely loads provided arguments
-    double U0 = loadArgument(argv[1]);
+    double U_0 = loadArgument(argv[1]);
     double R = loadArgument(argv[2]);
     double eps = loadArgument(argv[3]);
 
+    // Check if R is zero
+    if (R == 0) {
+        fputs("ERROR - R cannot be 0", stderr);
+        return -3;
+    }
 
+    // Calculate all values
+    double U_P = diode(U_0, R, eps);
+    double U_R = U_0 - U_P;
+    double I_R = U_R / R;
+    double I_P = I_R;
 
+    // Check for error
+    if (errno != 0) {
+        fputs("ERROR", stderr);
+        return -4;
+    }
+
+    // Print results
+    printf("Up=%g V\n", U_P);
+    printf("Ip=%g V\n", I_P);
     return 0;
 }
 
-// Checks argument and returns it's double value
+/**
+ * @param *char argument to be parsed
+ * @return parsed double
+ * @throws Parsing error if provided argument is of type double
+ *
+ * Checks argument and returns parsed value
+ */
 double loadArgument(char *arg) {
     // Creates pointer that will refer to first point of failure in strtod
     char *endptr = NULL;
@@ -52,5 +86,52 @@ double loadArgument(char *arg) {
         return -2;
     }
     // Returns parsed value
+    return ret;
+}
+
+/**
+ * @param u0 input voltage
+ * @param r resistance
+ * @param eps max deviation
+ * @return calculated U_P
+ *
+ * Calculates U_P based on provided parameters
+ */
+double diode(double u0, double r, double eps) {
+    // Initialize return variable
+    double ret = 0.;
+    // Get U_P Value
+    ret = calculateAccurateUP(u0, r, eps);
+    // Return U_P Value
+    return ret;
+}
+
+/**
+ *
+ * @param U_0 input voltage
+ * @param R resistance
+ * @param EPS max deviation
+ * @return approximated U_P
+ *
+ * Returns U_P that is approximated using bisection method
+ */
+double calculateAccurateUP(double U_0, double R, double EPS) {
+
+    return 0;
+}
+
+/**
+ *
+ * @param U_P voltage that is used
+ * @param U_0 input voltage
+ * @param R resistivity
+ * @return calculated result
+ *
+ * Returns the result with given U_P
+ */
+double getUPResult(double U_P, double U_0, double R) {
+    double ret = 0.;
+    // I_0 *  (exp(U_P / U_T) - 1) -  (U_0 - U_P / R) = 0
+    ret = I_0 * (exp(U_P / U_T) - 1) - (U_0 - U_P / R);
     return ret;
 }
